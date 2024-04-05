7bc1f7071a3306de3e21942eb79ea640f605e6e2
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-04-05 22:44:38 +0200

    HW07a ui v1.0

---
+++ HW07/HW07a_UI.c
@@ -0,0 +1,138 @@
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(int argc, char const *argv[]) {
+
+    int i, j, k, add;
+    char operation;
+    char newL;
+    int rowA, colA, rowB, colB;
+
+    scanf("%d%d", &rowA, &colA);
+
+    scanf("%c", &newL);
+    
+    int** matA = (int**) malloc(rowA* sizeof(int*));
+        for (i = 0; i < rowA; i++) matA[i] = (int*) malloc(colA* sizeof(int));
+
+    for (i = 0; i < rowA; i++) {
+        for (j = 0; j < colA; j++) {
+            scanf("%d", &matA[i][j]);
+            // printf("%d ", matA[i][j]);
+        }
+        scanf("%c", &newL);
+        // printf("\n");
+    }
+
+    scanf("%c", &operation);
+
+    if (!((operation == '+') | (operation == '-') | (operation == '*'))) {
+        for (i = 0; i < rowA; i++) free(matA[i]);
+        free(matA); 
+        fprintf(stderr, "Error: Chybny vstup!\n"); return 100;
+    }
+
+    scanf("%c", &newL);
+
+    scanf("%d%d", &rowB, &colB);
+
+    scanf("%c", &newL);
+
+    int** matB = (int**) malloc(rowB* sizeof(int*));
+        for (i = 0; i < rowB; i++) matB[i] = (int*) malloc(colB* sizeof(int));
+
+    for (i = 0; i < rowB; i++) {
+        for (j = 0; j < colB; j++) {
+            scanf("%d", &matB[i][j]);
+            // printf("%d ", matB[i][j]);
+        }
+        scanf("%c", &newL);
+        // printf("\n");
+    }
+
+
+    if ((operation == '+') | (operation == '-')) {
+        if ((rowA == rowB) & (colA == colB)) {
+            int rowC = rowA;
+            int colC = colA;
+
+            int** matC = (int**) malloc(rowC* sizeof(int));
+                for (i = 0; i < rowC; i++) matC[i] = (int*) malloc(colC* sizeof(int));
+
+        printf("%d %d\n", rowC, colC);
+
+        for (i = 0; i < rowC; i++)   {
+            for (j = 0; j < colC; j++)  {
+                if (operation == '+') matC[i][j] = matA[i][j] + matB[i][j];
+                else matC[i][j] = matA[i][j] - matB[i][j];
+
+                if (j == (colC - 1)) printf("%d", matC[i][j]);
+                else printf("%d ", matC[i][j]);
+            }
+            printf("\n");
+        }
+        for (i = 0; i < rowA; i++) free(matA[i]);
+        for (i = 0; i < rowB; i++) free(matB[i]);
+        for (i = 0; i < rowC; i++) free(matC[i]);
+
+        free(matA); free(matB); free(matC);
+
+        return 0;
+        }
+        else {  for (i = 0; i < rowA; i++) free(matA[i]);
+                for (i = 0; i < rowB; i++) free(matB[i]);
+                free(matA); free(matB); 
+                fprintf(stderr, "Error: Chybny vstup!\n"); return 100;}
+    }
+    else if (operation == '*') {
+        if (colA == rowB) {
+            int rowC = rowA;
+            int colC = colB;
+
+            printf("%d %d\n", rowC, colC);
+
+            int** matC = (int**) malloc(rowC* sizeof(int));
+                for (i = 0; i < rowC; i++) matC[i] = (int*) malloc(colC* sizeof(int));
+
+            for (i = 0; i < rowC; i++) {
+                for (j = 0; j < colC; j++) {
+                    add = 0;
+                    for (k = 0; k < rowB; k++) add += matA[i][k] * matB[k][j];
+
+                    matC[i][j] = add;
+                    if (j == (colC - 1)) printf("%d", matC[i][j]);
+                    else printf("%d ", matC[i][j]);
+                }
+            printf("\n");
+            }
+
+        for (i = 0; i < rowA; i++) free(matA[i]);
+        for (i = 0; i < rowB; i++) free(matB[i]);
+        for (i = 0; i < rowC; i++) free(matC[i]);           
+
+
+        free(matA); free(matB); free(matC);
+
+        return 0;
+        }
+        else {  for (i = 0; i < rowA; i++) free(matA[i]);
+                for (i = 0; i < rowB; i++) free(matB[i]);
+                free(matA); free(matB);
+                fprintf(stderr, "Error: Chybny vstup!\n"); return 100;}
+    }
+    else {  for (i = 0; i < rowA; i++) free(matA[i]);
+            for (i = 0; i < rowB; i++) free(matB[i]);
+            free(matA); free(matB); 
+            fprintf(stderr, "Error: Chybny vstup!\n"); return 100;}
+
+
+    for (i = 0; i < rowA; i++) free(matA[i]);
+    for (i = 0; i < rowB; i++) free(matB[i]);
+
+    free(matA); free(matB);
+
+    return 0;
+
+}
